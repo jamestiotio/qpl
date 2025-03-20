45bc8015387bc65b82bad63fd48d5608b50a05c9
Maria Zhukova <maria.zhukova@intel.com>
2025-03-19 19:32:39 -0700

    [fix] Adjust computations in qpl_get_safe_deflate_compression_buffer_size to use uint64_t to avoid integer overflow (#1191)

--- sources/c_api/utils_api.cpp
+++ sources/c_api/utils_api.cpp
@@ -24,47 +24,45 @@ extern "C" {
 QPL_FUN(uint32_t, qpl_get_safe_deflate_compression_buffer_size, (uint32_t source_size)) {
 
     // Define constants
-    const uint32_t BLOCK_HEADER_SIZE       = 5U;
-    const uint32_t FIRST_BLOCK_HEADER_SIZE = 1U;
-    const uint32_t OUTPUT_ACCUMULATOR_SIZE = 32U;
-    const uint32_t END_OF_BLOCK_SIZE       = 2U;
-
-    // Check if source_size + overhead is >= 0xFFFFFFFF
-    if (source_size >= (UINT32_MAX - FIRST_BLOCK_HEADER_SIZE - OUTPUT_ACCUMULATOR_SIZE - END_OF_BLOCK_SIZE)) {
-        return 0;
-    }
+    const uint8_t BLOCK_HEADER_SIZE       = 5U;
+    const uint8_t FIRST_BLOCK_HEADER_SIZE = 1U;
+    const uint8_t OUTPUT_ACCUMULATOR_SIZE = 32U;
+    const uint8_t END_OF_BLOCK_SIZE       = 2U;
 
     // Efficient divide by 65535, works for all values of x except for 0xFFFFFFFF
-    auto div_65535 = [](uint64_t x) -> uint32_t {
-        const uint32_t BITS_PER_BYTE          = 8U;
-        const uint32_t DIV_ROUND_UP_INCREMENT = 1U;
+    auto div_65535 = [](uint64_t x) -> uint64_t {
+        const uint8_t BITS_PER_BYTE          = 8U;
+        const uint8_t DIV_ROUND_UP_INCREMENT = 1U;
 
         const uint64_t y = (x >> (BITS_PER_BYTE * 2)) + x + DIV_ROUND_UP_INCREMENT;
-        return static_cast<uint32_t>(y >> (BITS_PER_BYTE * 2));
+        return (y >> (BITS_PER_BYTE * 2));
     };
 
 // Calculating stored block size, does not include first block header
 #ifdef _WIN32
-    auto stored_block_size = [&div_65535, BLOCK_HEADER_SIZE, FIRST_BLOCK_HEADER_SIZE](uint32_t osize) -> uint32_t {
+    auto stored_block_size = [&div_65535, BLOCK_HEADER_SIZE, FIRST_BLOCK_HEADER_SIZE](uint64_t osize) -> uint64_t {
 #else
-    auto stored_block_size = [&div_65535](uint32_t osize) -> uint32_t {
+    auto stored_block_size = [&div_65535](uint64_t osize) -> uint64_t {
 #endif
         const uint32_t ROUND_UP_OFFSET = UINT16_MAX - 1U;
 
         if (osize == 0) return BLOCK_HEADER_SIZE - FIRST_BLOCK_HEADER_SIZE;
-        const uint32_t num_blks = div_65535(osize + ROUND_UP_OFFSET); // round up
+        const uint64_t num_blks = div_65535(osize + ROUND_UP_OFFSET); // round up
         return osize + (num_blks * BLOCK_HEADER_SIZE) - FIRST_BLOCK_HEADER_SIZE;
     };
 
     // Estimate the maximum size needed for compression operations
     // Add 1 byte for the first block header, 32 bytes for the output accumulator, and 2 bytes for the EOB
-    uint32_t num_bytes =
+    uint64_t num_bytes =
             stored_block_size(source_size) + FIRST_BLOCK_HEADER_SIZE + OUTPUT_ACCUMULATOR_SIZE + END_OF_BLOCK_SIZE;
 
     // Round up to even number of bytes for BE16 encoding
     if (num_bytes % 2 != 0) { num_bytes++; }
 
-    return num_bytes;
+    // Check if the size is greater than UINT32_MAX since the function returns a uint32_t
+    if (num_bytes > UINT32_MAX) { return 0U; }
+
+    return static_cast<uint32_t>(num_bytes);
 }
 
 /**
